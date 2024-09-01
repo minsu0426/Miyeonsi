d4df6626378afe41d10973f73a7dc500af34e9f4
minsu0426 <kim62112879@gmail.com>
2024-09-02 01:36:18 +0900

    김민수

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public bool isScene;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
