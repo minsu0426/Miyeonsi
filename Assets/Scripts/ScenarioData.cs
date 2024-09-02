be50df7a91edc34b0f203f26bfbb94b355d6fcb8
W_H <hju013961@gmail.com>
2024-09-02 14:49:17 +0900

    주현성 : 폴더 및 스크립트 추가

---
+++ Assets/Scripts/ScenarioData.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[CreateAssetMenu(fileName = "NewScenarioData", menuName = "ScenarioData", order = 51)]
+public class ScenarioData : ScriptableObject
+{
+    [Header("기본 텍스트설정")]
+    [Multiline(3)]
+    public string text;
+    public float textSpeed;
+
+    [Header("선택지 여부")]
+    public bool hasOption;
+    public TMP_Text[] optionTmp;
+    public string[] optionText;
+    public ScenarioData[] optionResult;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
