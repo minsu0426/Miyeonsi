4c35f9a9488a329bb82d5d57355614ff250a8c01
gari <happykyk0103@naver.com>
2024-09-02 21:36:45 +0900

    김용권 - 넘버패드 깜빡임 추가

--- Assets/Scripts/BombDefuse.cs
+++ Assets/Scripts/BombDefuse.cs
@@ -3,30 +3,47 @@ using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.UI;
+using Unity.VisualScripting;
+using System;
+using Random = UnityEngine.Random;
 
 public class BombDefuse : MonoBehaviour
 {
-    public Button[] buttons;
     public int[] correctSequence;
     private int currentStep = 0;
     private float timeRemaining;
     private bool defused = false;
+    private int Sequence_Rage;
     private bool flag = false;
     public Text timerText;
     public GameObject gameOverSereen;
-    public GameObject successScreen;
+    public Button[] buttons;
+    public Color blinkColor = Color.yellow;
+    public float blinkDuration = 0.5f;
+    public float delayBetweenBlinks = 0.5f;
+    public float startDelay = 2f;
+    private Color[] originalColors;
+
     void Start()
     {
-        timeRemaining = 30f;
+        timeRemaining = 32f;
         UpdateTimerText();
-        int Sequence_Rage = Random.Range(4,6);
+        Sequence_Rage = Random.Range(6,10);
         correctSequence = new int[Sequence_Rage];
+        originalColors = new Color[9];
 
-        for(int i = 0; i<Sequence_Rage; i++)
+        for (int i = 0; i<Sequence_Rage; i++)
         {
-            correctSequence[i] = Random.Range(1, 9);
+            correctSequence[i] = Random.Range(0, 9);
             Debug.Log(correctSequence[i]);
+            
         }
+        for (int i = 0; i < 9; i++)
+        {
+            originalColors[i] = buttons[i].GetComponent<Image>().color;
+        }
+
+        StartCoroutine(ShowSequence());
     }
 
     void Update()
@@ -78,13 +95,32 @@ public class BombDefuse : MonoBehaviour
     void GameOver()
     {
         Debug.Log("Game Over");
-       // gameOverSereen.SetActive(true);
     }
     
      void DefuseBomb()
     {
         defused = true;
         Debug.Log("Defused");
-        //successScreen.SetActive(true);
+    }
+
+    IEnumerator ShowSequence()
+    {
+        yield return new WaitForSeconds(startDelay);
+
+        for (int i = 0; i < Sequence_Rage; i++)
+        {
+            int index = correctSequence[i];
+            StartCoroutine(BlinkButton(buttons[index]));
+            yield return new WaitForSeconds(blinkDuration + delayBetweenBlinks);
+        }
+    }
+
+
+    IEnumerator BlinkButton(Button button)
+    {
+        Image buttonImage = button.GetComponent<Image>();
+        buttonImage.color = blinkColor;
+        yield return new WaitForSeconds(blinkDuration);
+        buttonImage.color = originalColors[Array.IndexOf(buttons, button)];
     }
 }
