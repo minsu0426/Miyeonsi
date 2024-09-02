ffd599f5a6e103e3dd122a1e330ab5c84f58b758
Minsu <kim62112879@gmail.com>
2024-09-03 08:39:59 +0900

    .

--- Assets/Scripts/ExitButton.cs
+++ Assets/Scripts/ExitButton.cs
@@ -4,13 +4,13 @@ public class ExitButton : MonoBehaviour
 {
     public void OnExitPressButton()
     {
-        if (Application.isPlaying)
+        /*if (Application.isPlaying)
         {
             UnityEditor.EditorApplication.isPlaying = false;
         }
         else
-        {
+        {*/
             Application.Quit();
-        }
+        //}
     }
 }
