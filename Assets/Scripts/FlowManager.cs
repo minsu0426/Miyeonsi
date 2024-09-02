94e0a005bcf3bc0959acd9c7ee59837d8ea6b454
W_H <hju013961@gmail.com>
2024-09-02 14:56:30 +0900

    주현성: 폴더

--- Assets/Scripts/FlowManager.cs
+++ Assets/Scripts/FlowManager.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class FlowManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject textBox;
+    public GameObject optionPrefab;
 
-    // Update is called once per frame
-    void Update()
+    public ScenarioData scenarioData;
+    
+    void ShowText()
     {
-        
+
     }
 }
