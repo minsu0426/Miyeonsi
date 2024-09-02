cf519d58817ef244f3af295b164776f42bd704de
Minsu <kim62112879@gmail.com>
2024-09-02 15:33:52 +0900

    김민수 - 버튼 온클릭 추가

--- Assets/Scripts/ExitButton.cs
+++ Assets/Scripts/ExitButton.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ExitButton : MonoBehaviour
 {
-    public void OnPressButton()
+    public void OnExitPressButton()
     {
         if (Application.isPlaying)
         {
